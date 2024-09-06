00f591c3240cbc513e2eef3bf8472769b1afb13f
Badrbernane <151386759+Badrbernane@users.noreply.github.com>
2024-09-06 18:15:56 +0100

    h

--- Models/PerssComp.cs
+++ Models/PerssComp.cs
@@ -3,8 +3,8 @@ namespace MonBackendApp.Models
     public class PerssComp
     {
         public int idPersonnes { get; set; }
-        public Personne? personne { get; set; }
+        public Personne personne { get; set; } = null!;
         public int idCompétences { get; set; }
-        public Compétence? Compétence { get; set; }
+        public Compétence Compétence { get; set; } = null!;
     }
 }
